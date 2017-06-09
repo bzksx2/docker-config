6d719885abd03910090bbce375288478e90cfc9f
userya <ctg47@qq.com>
2017-06-09 15:50:21 +0800

    u

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 
-# cd /home/admin/app && git pull https://github.com/userya/release-package-config-s.git
+cd /home/admin/app && git pull https://github.com/userya/release-package-config-s.git
 nginx -c /home/admin/app/sys-config/nginx.conf 
-# java -jar /home/admin/app/java/api-1.0-SNAPSHOT.jar &
+java -jar /home/admin/app/java/api-1.0-SNAPSHOT.jar &
